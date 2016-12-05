f143d2f6cadd02f4caa2cf5360cac73129df40c9
Clayton Coleman <ccoleman@redhat.com>
2016-12-05 12:08:16 -0500

    Remove inventory source from the entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,4 @@
 export PATH=/usr/local/install/google-cloud-sdk/bin:$PATH
 export CONTEXT_DIR=/usr/local/install/data
 gcloud auth activate-service-account --key-file=/usr/local/install/data/gce.json
-source "${CONTEXT_DIR}/inventory.sh"
 exec "$@"
