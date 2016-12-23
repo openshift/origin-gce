8d312c01c34a8ef88a88b158cc9a5ba12987a724
Clayton Coleman <ccoleman@redhat.com>
2016-12-23 10:00:18 -0500

    Ansible issue 16857 requires absolute inventory path

--- run.sh
+++ run.sh
@@ -5,5 +5,9 @@
 #
 set -euo pipefail
 
+# meta refresh_inventory has a bug in 2.2.0 where it uses relative path
+# remove when fixed
+export ANSIBLE_INVENTORY=$(pwd)/inventory.sh
+
 CONTEXT_DIR="${CONTEXT_DIR:-/usr/local/install/data}"
 ansible-playbook -e "install_var=${CONTEXT_DIR}/ansible-config.yml" "$@"
