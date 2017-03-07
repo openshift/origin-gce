4805c8e131ed6defd21ad3fef1dc7d741c192428
Clayton Coleman <ccoleman@redhat.com>
2017-03-07 11:17:26 -0500

    Remove gce_service_account construct and read from keyfile
    No need for a separate variable

--- playbooks/roles/dynamic-inventory/templates/inventory.j2.sh
+++ playbooks/roles/dynamic-inventory/templates/inventory.j2.sh
@@ -2,7 +2,7 @@
 
 export GCE_PROJECT="{{ gce_project_id }}"
 export GCE_ZONE="{{ gce_zone_name }}"
-export GCE_EMAIL="{{ gce_service_account }}"
+export GCE_EMAIL="{{ (lookup('file', playbook_dir + '/files/' + gce_service_account_keyfile ) | from_json ).client_email }}"
 export GCE_PEM_FILE_PATH="/tmp/gce.pem"
 export INVENTORY_IP_TYPE="{{ inventory_ip_type }}"
 export GCE_TAGGED_INSTANCES="{{ provision_prefix }}ocp"
